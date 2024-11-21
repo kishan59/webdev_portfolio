687efc15b26017cd86a0d6a2a39fccd9458f92b7
Kishan Lamba <kishanlamba59@gmail.com>
2024-11-21 16:01:09 +0530

    Final web dev portfolio template complete

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -4,7 +4,7 @@ import { companies, testimonials } from '@/data'
 
 const Clients = () => {
   return (
-    <div id='clients' className='py-20'>
+    <div id='testimonials' className='py-20'>
         <h1 className='heading'>
             Kind words from
             <span className='text-purple'> satisfied clients</span>
